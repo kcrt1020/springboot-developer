d2b8b9a3260e92dda9a788c1b6be7d52b4de655b
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-26 00:02:51 +0900

    글목록 view

---
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.example.springbootdeveloper.controller;
+
+import com.example.springbootdeveloper.dto.ArticleListViewResponse;
+import com.example.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    
+    private final BlogService blogService;
+    
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);   // 블로그 글 리스트 저장
+        
+        return "articleList";   // articleList.html 뷰 조회
+    }
+}
