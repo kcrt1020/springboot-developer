70e3cbc15ef4d9c0959d930fce6d97bc272ac27a
kcrt1020 <67083727+kcrt1020@users.noreply.github.com>
2023-08-26 00:24:28 +0900

    글 view

--- src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.springbootdeveloper.controller;
 
+import com.example.springbootdeveloper.domain.Article;
 import com.example.springbootdeveloper.dto.ArticleListViewResponse;
+import com.example.springbootdeveloper.dto.ArticleViewResponse;
 import com.example.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);   // 블로그 글 리스트 저장
         
         return "articleList";   // articleList.html 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
